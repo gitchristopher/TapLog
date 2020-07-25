9b5e1f72358afe854790f0a2f2f3827fb4a04736
Chris <chris.linnan@gmail.com>
2020-07-25 16:11:01 +1000

    Device entity migration

---
+++ src/Infrastructure/Persistence/Migrations/20200725060816_AlterDeviceEntity.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TapLog.Infrastructure.Persistence.Migrations
+{
+    public partial class AlterDeviceEntity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Latitude",
+                table: "Devices",
+                maxLength: 16,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Longitude",
+                table: "Devices",
+                maxLength: 16,
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Zone",
+                table: "Devices",
+                maxLength: 16,
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Latitude",
+                table: "Devices");
+
+            migrationBuilder.DropColumn(
+                name: "Longitude",
+                table: "Devices");
+
+            migrationBuilder.DropColumn(
+                name: "Zone",
+                table: "Devices");
+        }
+    }
+}
