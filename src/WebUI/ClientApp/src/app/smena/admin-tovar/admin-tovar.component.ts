c5c59b3b704b9910d38535742d219db1e095dadc
Chris <chris.linnan@gmail.com>
2020-07-30 00:07:17 +1000

    Refactor Products

--- src/WebUI/ClientApp/src/app/smena/admin-tovar/admin-tovar.component.ts
+++ src/WebUI/ClientApp/src/app/smena/admin-tovar/admin-tovar.component.ts
@@ -11,28 +11,25 @@ import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms'
   styleUrls: ['./admin-tovar.component.css']
 })
 export class AdminTovarComponent implements OnInit {
+  debug = false;
+
   dataSource: ProductDto[] = [];
   columnList: string[] = ['id', 'name', 'edit', 'delete'];
-
-  updateModalRef: BsModalRef;
-  selectedEntity = new ProductDto();
-  createModalRef: BsModalRef;
-  newEntity = new ProductDto();
-  modalEditor: IModal = {title: 'Editor', errors: null };
-
-  ufg: FormGroup;
-  cfg: FormGroup;
-  debug = false;
   @ViewChild(MatTable) table: MatTable<any>;
+
+  updateForm: FormGroup;
+  createForm: FormGroup;
+  modalRef: BsModalRef;
+  modalEditor: IModal = {title: 'Editor', errors: null };
 
   constructor(private productsClient: ProductsClient, private modalService: BsModalService) { }
 
   ngOnInit() {
-    this.ufg = new FormGroup({
+    this.updateForm = new FormGroup({
       name: new FormControl('', [Validators.required, Validators.minLength(1), Validators.maxLength(32)]),
       id: new FormControl('', [Validators.required]),
     });
-    this.cfg = new FormGroup({
+    this.createForm = new FormGroup({
       name: new FormControl('', [Validators.required, Validators.minLength(1), Validators.maxLength(32)]),
     });
   }
@@ -40,83 +37,79 @@ export class AdminTovarComponent implements OnInit {
   refresh() {
     this.productsClient.getAll().subscribe(
       result => {
-        this.dataSource = result; console.log(result);
+        this.dataSource = result;
       },
       error => console.error(error)
     );
   }
 
-  updateCancelled() {
-    this.updateModalRef.hide();
-    this.modalEditor.errors = null;
-  }
-  createCancelled() {
-    this.createModalRef.hide();
-    this.modalEditor.errors = null;
+  openUpdateModal(entity: ProductDto, template: TemplateRef<any>) {
+    this.updateForm.patchValue(entity);
+    this.updateForm.get('id').disable();
+    this.modalEditor.title = 'Update Product: ' + entity.name;
+    this.modalRef = this.modalService.show(template);
   }
 
   updateEntity() {
-    this.selectedEntity.name = this.ufg.value.name;
-    const updatedEntity = UpdateProductCommand.fromJS(this.selectedEntity);
+    const updateEntityCommand = UpdateProductCommand.fromJS(this.updateForm.getRawValue());
 
-    this.productsClient.update(updatedEntity.id, updatedEntity).subscribe(
+    this.productsClient.update(updateEntityCommand.id, updateEntityCommand).subscribe(
         result => {
-          // this.refresh();
-          const index = this.dataSource.findIndex(x => x.id === this.selectedEntity.id);
-          this.dataSource.splice(index, 1, this.selectedEntity);
+          const index = this.dataSource.findIndex(x => x.id === updateEntityCommand.id);
+          const updatedEntity = this.createEntityFromForm(this.updateForm);
+          this.dataSource.splice(index, 1, updatedEntity);
           this.table.renderRows();
-          this.modalEditor.errors = null;
-          this.updateModalRef.hide();
+          this.closeModal(this.updateForm);
         },
         error => {
-            const errors = JSON.parse(error.response);
-            console.error('Error while updating the stage.');
-            if (errors && errors.Title) {
-                this.modalEditor.errors.push(errors.Title[0]);
-            }
-            setTimeout(() => document.getElementById('title').focus(), 250);
+          this.addErrorsToModal(error);
         }
     );
   }
-  createEntity() {
-    this.newEntity.name = this.cfg.value.name;
-    const entity = CreateProductCommand.fromJS(this.newEntity);
 
-    this.productsClient.create(entity).subscribe(
+  openCreateModal(template: TemplateRef<any>) {
+    this.modalEditor.title = 'Create New Product';
+    this.modalRef = this.modalService.show(template);
+  }
+
+  saveEntity() {
+    const newEntityCommand = CreateProductCommand.fromJS(this.createForm.getRawValue());
+
+    this.productsClient.create(newEntityCommand).subscribe(
         result => {
           if (result > 0) {
-            const index = this.dataSource.findIndex(x => x.id === this.selectedEntity.id);
-            this.newEntity.id = result;
-            this.dataSource.push(ProductDto.fromJS(this.newEntity));
+            const entity = this.createEntityFromForm(this.createForm);
+            entity.id = result;
+            this.dataSource.push(entity);
             this.table.renderRows();
+            this.closeModal(this.createForm);
+          } else {
+            this.modalEditor.errors.push('An error occured while saving the new Product.');
           }
-          this.modalEditor.errors = null;
-          this.createModalRef.hide();
         },
         error => {
-            const errors = JSON.parse(error.response);
-            console.error('Error while creating a Product.');
-            if (errors && errors.Title) {
-                this.modalEditor.errors.push(errors.Title[0]);
-            }
-            setTimeout(() => document.getElementById('title').focus(), 250);
+          this.addErrorsToModal(error);
         }
     );
   }
 
-  openUpdateModal(entity: ProductDto, template: TemplateRef<any>) {
-    this.selectedEntity = ProductDto.fromJS(this.dataSource.find(x => x.id === entity.id));
-    this.modalEditor.title = 'Update Product: ' + this.selectedEntity.name;
-    this.ufg.get('name').setValue(this.selectedEntity.name);
-    this.ufg.get('id').setValue(this.selectedEntity.id);
-    this.ufg.get('id').disable();
-    this.updateModalRef = this.modalService.show(template);
+  private createEntityFromForm(form: FormGroup): ProductDto {
+    const entity = ProductDto.fromJS(form.getRawValue());
+    entity.cards = [];
+    return entity;
   }
-  openCreateModal(template: TemplateRef<any>) {
-    this.newEntity = new ProductDto();
-    this.modalEditor.title = 'Create New Product';
-    this.cfg.get('name').setValue('');
-    this.createModalRef = this.modalService.show(template);
+
+  private addErrorsToModal(error: any) {
+    const errors = JSON.parse(error.response);
+    if (errors && errors.title) {
+      this.modalEditor.errors = [errors];
+    }
+  }
+
+  closeModal(form: FormGroup) {
+    form.reset();
+    this.modalRef.hide();
+    this.modalEditor.errors = null;
   }
 
   deleteEntity(id: number) {
