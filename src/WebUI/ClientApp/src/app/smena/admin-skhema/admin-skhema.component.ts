34e3d719a685919b11da0f1ee7c0e75c41cec471
Chris <chris.linnan@gmail.com>
2020-07-29 23:50:36 +1000

    Refactor Suppliers

--- src/WebUI/ClientApp/src/app/smena/admin-skhema/admin-skhema.component.ts
+++ src/WebUI/ClientApp/src/app/smena/admin-skhema/admin-skhema.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit, TemplateRef, ViewChild } from '@angular/core';
-import { StageDto, StagesClient, IUpdateStageCommand, UpdateStageCommand, CreateStageCommand, SupplierDto, SuppliersClient, UpdateSupplierCommand, CreateSupplierCommand } from 'src/app/taplog-api';
+import { SupplierDto, SuppliersClient, UpdateSupplierCommand, CreateSupplierCommand } from 'src/app/taplog-api';
 import { BsModalRef, BsModalService } from 'ngx-bootstrap';
 import { IModal } from 'src/_interfaces/modal';
 import { MatTable } from '@angular/material/table';
-import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-admin-skhema',
@@ -11,28 +11,25 @@ import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms'
   styleUrls: ['./admin-skhema.component.css']
 })
 export class AdminSkhemaComponent implements OnInit {
+  debug = false;
+
   dataSource: SupplierDto[] = [];
   columnList: string[] = ['id', 'name', 'edit', 'delete'];
-
-  updateModalRef: BsModalRef;
-  selectedEntity = new SupplierDto();
-  createModalRef: BsModalRef;
-  newEntity = new SupplierDto();
-  modalEditor: IModal = {title: 'Editor', errors: null };
-
-  ufg: FormGroup;
-  cfg: FormGroup;
-  debug = false;
   @ViewChild(MatTable) table: MatTable<any>;
+
+  updateForm: FormGroup;
+  createForm: FormGroup;
+  modalRef: BsModalRef;
+  modalEditor: IModal = {title: 'Editor', errors: null };
 
   constructor(private suppliersClient: SuppliersClient, private modalService: BsModalService) { }
 
   ngOnInit() {
-    this.ufg = new FormGroup({
+    this.updateForm = new FormGroup({
       name: new FormControl('', [Validators.required, Validators.minLength(1), Validators.maxLength(32)]),
       id: new FormControl('', [Validators.required]),
     });
-    this.cfg = new FormGroup({
+    this.createForm = new FormGroup({
       name: new FormControl('', [Validators.required, Validators.minLength(1), Validators.maxLength(32)]),
     });
   }
@@ -40,83 +37,79 @@ export class AdminSkhemaComponent implements OnInit {
   refresh() {
     this.suppliersClient.getAll().subscribe(
       result => {
-        this.dataSource = result; console.log(result);
+        this.dataSource = result;
       },
       error => console.error(error)
     );
   }
 
-  updateCancelled() {
-    this.updateModalRef.hide();
-    this.modalEditor.errors = null;
-  }
-  createCancelled() {
-    this.createModalRef.hide();
-    this.modalEditor.errors = null;
+  openUpdateModal(entity: SupplierDto, template: TemplateRef<any>) {
+    this.updateForm.patchValue(entity);
+    this.updateForm.get('id').disable();
+    this.modalEditor.title = 'Update Supplier: ' + entity.name;
+    this.modalRef = this.modalService.show(template);
   }
 
   updateEntity() {
-    this.selectedEntity.name = this.ufg.value.name;
-    const updatedEntity = UpdateSupplierCommand.fromJS(this.selectedEntity);
+    const updateEntityCommand = UpdateSupplierCommand.fromJS(this.updateForm.getRawValue());
 
-    this.suppliersClient.update(updatedEntity.id, updatedEntity).subscribe(
-        result => {
-          // this.refresh();
-          const index = this.dataSource.findIndex(x => x.id === this.selectedEntity.id);
-          this.dataSource.splice(index, 1, this.selectedEntity);
-          this.table.renderRows();
-          this.modalEditor.errors = null;
-          this.updateModalRef.hide();
-        },
-        error => {
-            const errors = JSON.parse(error.response);
-            console.error('Error while updating the stage.');
-            if (errors && errors.Title) {
-                this.modalEditor.errors.push(errors.Title[0]);
-            }
-            setTimeout(() => document.getElementById('title').focus(), 250);
-        }
+    this.suppliersClient.update(updateEntityCommand.id, updateEntityCommand).subscribe(
+      result => {
+        const index = this.dataSource.findIndex(x => x.id === updateEntityCommand.id);
+        const updatedEntity = this.createEntityFromForm(this.updateForm);
+        this.dataSource.splice(index, 1, updatedEntity);
+        this.table.renderRows();
+        this.closeModal(this.updateForm);
+      },
+      error => {
+        this.addErrorsToModal(error);
+      }
     );
   }
-  createEntity() {
-    this.newEntity.name = this.cfg.value.name;
-    const entity = CreateSupplierCommand.fromJS(this.newEntity);
 
-    this.suppliersClient.create(entity).subscribe(
+  openCreateModal(template: TemplateRef<any>) {
+    this.modalEditor.title = 'Create New Supplier';
+    this.modalRef = this.modalService.show(template);
+  }
+
+  saveEntity() {
+    const newEntityCommand = CreateSupplierCommand.fromJS(this.createForm.getRawValue());
+
+    this.suppliersClient.create(newEntityCommand).subscribe(
         result => {
           if (result > 0) {
-            const index = this.dataSource.findIndex(x => x.id === this.selectedEntity.id);
-            this.newEntity.id = result;
-            this.dataSource.push(SupplierDto.fromJS(this.newEntity));
+            const entity = this.createEntityFromForm(this.createForm);
+            entity.id = result;
+            this.dataSource.push(entity);
             this.table.renderRows();
+            this.closeModal(this.createForm);
+          } else {
+            this.modalEditor.errors.push('An error occured while saving the new Supplier.');
           }
-          this.modalEditor.errors = null;
-          this.createModalRef.hide();
         },
         error => {
-            const errors = JSON.parse(error.response);
-            console.error('Error while creating a supplier.');
-            if (errors && errors.Title) {
-                this.modalEditor.errors.push(errors.Title[0]);
-            }
-            setTimeout(() => document.getElementById('title').focus(), 250);
+          this.addErrorsToModal(error);
         }
     );
   }
 
-  openUpdateModal(entity: SupplierDto, template: TemplateRef<any>) {
-    this.selectedEntity = SupplierDto.fromJS(this.dataSource.find(x => x.id === entity.id));
-    this.modalEditor.title = 'Update Supplier: ' + this.selectedEntity.name;
-    this.ufg.get('name').setValue(this.selectedEntity.name);
-    this.ufg.get('id').setValue(this.selectedEntity.id);
-    this.ufg.get('id').disable();
-    this.updateModalRef = this.modalService.show(template);
+  private createEntityFromForm(form: FormGroup): SupplierDto {
+    const entity = SupplierDto.fromJS(form.getRawValue());
+    entity.cards = [];
+    return entity;
   }
-  openCreateModal(template: TemplateRef<any>) {
-    this.newEntity = new SupplierDto();
-    this.modalEditor.title = 'Create New Supplier';
-    this.cfg.get('name').setValue('');
-    this.createModalRef = this.modalService.show(template);
+
+  private addErrorsToModal(error: any) {
+    const errors = JSON.parse(error.response);
+    if (errors && errors.title) {
+      this.modalEditor.errors = [errors];
+    }
+  }
+
+  closeModal(form: FormGroup) {
+    form.reset();
+    this.modalRef.hide();
+    this.modalEditor.errors = null;
   }
 
   deleteEntity(id: number) {
