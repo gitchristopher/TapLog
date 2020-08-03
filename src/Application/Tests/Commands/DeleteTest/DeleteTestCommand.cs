617745bdc310c6cbe7bc09f2c38a4e8a3b3fc513
Chris <chris.linnan@gmail.com>
2020-08-03 19:56:34 +1000

    Dealing with orphan tests

--- src/Application/Tests/Commands/DeleteTest/DeleteTestCommand.cs
+++ src/Application/Tests/Commands/DeleteTest/DeleteTestCommand.cs
@@ -36,15 +36,36 @@ namespace TapLog.Application.Tests.Commands.DeleteTest
         public async Task<Unit> Handle(DeleteTestCommand request, CancellationToken cancellationToken)
         {
             // Retrieve the entity
-            var entity = await _context.StageTests.Include(t => t.Test).Where(x => x.StageId == request.StageId).Where(x => x.TestId == request.TestId).FirstOrDefaultAsync();
+            //var entity = await _context.StageTests.Include(t => t.Test).Where(x => x.StageId == request.StageId).Where(x => x.TestId == request.TestId).FirstOrDefaultAsync();
 
-            if (entity == null)
+            // Get all the stagetests that realate to the test to be deleted
+            // Select the single stagetest to be deleted
+            // If it doesnt exist throw an error
+            // Check to see if the list only has one entry and if it does get the test entity then delete it
+            // If it doesnt, dont delete the test as it exists in other stages
+            // Just delete the stagetest for the requested test.
+            // this will cascade and delete the executions and taps for that stagetest.
+
+            var entities = await _context.StageTests.Include(t => t.Test).Where(x => x.TestId == request.TestId).ToListAsync();
+            if (entities.Count == 0)
             {
                 throw new NotFoundException(nameof(Test), request.TestId);
             }
 
+            var specificStageTest = entities.Where(x => x.StageId == request.StageId).FirstOrDefault();
+            if (specificStageTest == null)
+            {
+                throw new NotFoundException(nameof(Stage), request.StageId);
+            }
+
+            if (entities.Count == 1)
+            {
+                var orphanTest = await _context.Tests.FindAsync(specificStageTest.TestId);
+                _context.Tests.Remove(orphanTest);
+            }
+
             // Delete the entity
-            _context.StageTests.Remove(entity);
+            _context.StageTests.Remove(specificStageTest);
 
             await _context.SaveChangesAsync(cancellationToken);
 
