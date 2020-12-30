051b64b253a7179cb3ab5f39a13c73a85e7a2a83
Chris <chris.linnan@gmail.com>
2020-12-31 09:14:17 +1000

    Separate out material modules and other minor changes

---
+++ src/WebUI/ClientApp/src/_modules/material.module.ts
@@ -0,0 +1,93 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatSortModule } from '@angular/material/sort';
+import { MatTableModule } from '@angular/material/table';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatTableExporterModule } from 'mat-table-exporter';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatCheckboxModule,
+    MatButtonToggleModule,
+    MatBadgeModule,
+    MatAutocompleteModule,
+    MatDialogModule,
+    MatMenuModule,
+    MatPaginatorModule,
+    MatProgressSpinnerModule,
+    MatRadioModule,
+    MatSelectModule,
+    MatSlideToggleModule,
+    MatSliderModule,
+    MatSnackBarModule,
+    MatSortModule,
+    MatTableModule,
+    MatExpansionModule,
+    MatNativeDateModule,
+    MatTableExporterModule,
+  ],
+  exports: [
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatCheckboxModule,
+    MatButtonToggleModule,
+    MatBadgeModule,
+    MatAutocompleteModule,
+    MatDialogModule,
+    MatMenuModule,
+    MatPaginatorModule,
+    MatProgressSpinnerModule,
+    MatRadioModule,
+    MatSelectModule,
+    MatSlideToggleModule,
+    MatSliderModule,
+    MatSnackBarModule,
+    MatSortModule,
+    MatTableModule,
+    MatExpansionModule,
+    MatNativeDateModule,
+    MatTableExporterModule,
+  ]
+})
+export class MaterialModule { }
--- src/WebUI/ClientApp/src/app/app.module.ts
+++ src/WebUI/ClientApp/src/app/app.module.ts
@@ -1,33 +1,53 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { TimepickerModule } from 'ngx-bootstrap/timepicker';
+import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
+import { ModalModule } from 'ngx-bootstrap/modal';
+import { ButtonsModule } from 'ngx-bootstrap';
+import { environment } from '../environments/environment';
 
+// MISC
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { TodoComponent } from './todo/todo.component';
+import { HomeComponent } from './home/home.component';
 import { ApiAuthorizationModule } from 'src/api-authorization/api-authorization.module';
 import { AuthorizeGuard } from 'src/api-authorization/authorize.guard';
 import { AuthorizeInterceptor } from 'src/api-authorization/authorize.interceptor';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ModalModule } from 'ngx-bootstrap/modal';
-import { ButtonsModule } from 'ngx-bootstrap';
-import { ReactiveFormsModule } from '@angular/forms';
+import { MaterialModule } from 'src/_modules/material.module';
+import { MAT_DATE_LOCALE } from '@angular/material/core';
 
+// FIRST ITERATION OF TAP LOGGER
 import { TapLoggerComponent } from './tap-logger/tap-logger.component';
-import { HomeComponent } from './home/home.component';
 
+// SECOND ITERATION OF TAP LOGGER
 import { TapperComponent } from './tapper/tapper.component';
 import { StageListComponent } from './tapper/stage-list/stage-list.component';
 import { TestListComponent } from './tapper/test-list/test-list.component';
 import { ExecuteListComponent } from './tapper/execute-list/execute-list.component';
 import { LogTapComponent } from './tapper/log-tap/log-tap.component';
 import { TapListComponent } from './tapper/tap-list/tap-list.component';
+
+// THIRD ITERATION OF TAP LOGGER
+import { ZapolnitComponent } from './zapolnit/zapolnit.component';
+import { SpisokFazComponent } from './zapolnit/spisok-faz/spisok-faz.component';
+import { SpisokKazneyComponent } from './zapolnit/spisok-kazney/spisok-kazney.component';
+import { SpisokSobytiyComponent } from './zapolnit/spisok-sobytiy/spisok-sobytiy.component';
+import { SpisokTestovComponent } from './zapolnit/spisok-testov/spisok-testov.component';
+import { ZapisSobytiyComponent } from './zapolnit/zapis-sobytiy/zapis-sobytiy.component';
+
+// QUERY AREA
 import { StolComponent } from './stol/stol.component';
 import { ZaprosComponent } from './stol/zapros/zapros.component';
+
+// ADMIN AREA
 import { SmenaComponent } from './smena/smena.component';
 import { AdminEtapComponent } from './smena/admin-etap/admin-etap.component';
 import { AdminSkhemaComponent } from './smena/admin-skhema/admin-skhema.component';
@@ -36,51 +56,15 @@ import { AdminTovarComponent } from './smena/admin-tovar/admin-tovar.component';
 import { AdminUstroystvoComponent } from './smena/admin-ustroystvo/admin-ustroystvo.component';
 import { AdminOtkrytkaComponent } from './smena/admin-otkrytka/admin-otkrytka.component';
 
+// PIPES
 import { ResultEnumPipe } from '../_pipes/ResultEnumPipe';
 import { ExpectedEnumPipe } from '../_pipes/ExpectedEnumPipe';
 import { TapActionToArrayPipe } from '../_pipes/TapActionToArray.pipe';
 import { TapActionEnumPipe } from '../_pipes/TapActionEnum.pipe';
 import { TesterNamePipe } from '../_pipes/TesterName.pipe';
 
-import { ZapolnitComponent } from './zapolnit/zapolnit.component';
-import { SpisokFazComponent } from './zapolnit/spisok-faz/spisok-faz.component';
-import { SpisokKazneyComponent } from './zapolnit/spisok-kazney/spisok-kazney.component';
-import { SpisokSobytiyComponent } from './zapolnit/spisok-sobytiy/spisok-sobytiy.component';
-import { SpisokTestovComponent } from './zapolnit/spisok-testov/spisok-testov.component';
-import { ZapisSobytiyComponent } from './zapolnit/zapis-sobytiy/zapis-sobytiy.component';
-
-import { TimepickerModule } from 'ngx-bootstrap/timepicker';
-import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
-import { MatButtonModule } from '@angular/material/button';
-import { MatInputModule } from '@angular/material/input';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatSelectModule } from '@angular/material/select';
-import { MatIconModule } from '@angular/material/icon';
-import { MatListModule } from '@angular/material/list';
-import { MatRadioModule } from '@angular/material/radio';
-import { MatButtonToggleModule } from '@angular/material/button-toggle';
-import { MatExpansionModule } from '@angular/material/expansion';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-import { MatAutocompleteModule } from '@angular/material/autocomplete';
-import { MatTableModule } from '@angular/material/table';
-import { MatPaginatorModule } from '@angular/material/paginator';
-import { MatSortModule } from '@angular/material/sort';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import { MatDatepickerModule } from '@angular/material/datepicker';
-import { MatNativeDateModule } from '@angular/material/core';
-import { MatCheckboxModule } from '@angular/material/checkbox';
-import { MAT_DATE_LOCALE } from '@angular/material/core';
-import { MatBadgeModule } from '@angular/material/badge';
-import { MatMenuModule } from '@angular/material/menu';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatDialogModule } from '@angular/material/dialog';
-import { MatSnackBarModule } from '@angular/material/snack-bar';
-
-import { MatTableExporterModule } from 'mat-table-exporter';
-
+// NGRX STATE
 import { StoreModule } from '@ngrx/store';
-import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-import { environment } from '../environments/environment';
 import { EffectsModule } from '@ngrx/effects';
 import { fazReducer } from '../app/zapolnit/spisok-faz/spisok-faz.reducers';
 import { testovReducer } from '../app/zapolnit/spisok-testov/spisok-testov.reducers';
@@ -90,41 +74,49 @@ import { FazEffects } from '../app/zapolnit/spisok-faz/spisok-faz.effects';
 import { TestovEffects } from '../app/zapolnit/spisok-testov/spisok-testov.effects';
 import { KazneyEffects } from './zapolnit/spisok-kazney/spisok-kazney.effects';
 import { SobytiyEffects } from './zapolnit/spisok-sobytiy/spisok-sobytiy.effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+
 
 
 @NgModule({
    declarations: [
       AppComponent,
       NavMenuComponent,
-      TapperComponent,
       TodoComponent,
       HomeComponent,
+      // 1ST ITERATION OF TAP LOGGER
+      TapperComponent,
+      // 2ND ITERATION OF TAP LOGGER
       TapLoggerComponent,
       StageListComponent,
       TestListComponent,
       ExecuteListComponent,
       LogTapComponent,
       TapListComponent,
-      ResultEnumPipe,
-      ExpectedEnumPipe,
-      TapActionEnumPipe,
-      TapActionToArrayPipe,
-      TesterNamePipe,
+      // 3RD ITERATION OF TAP LOGGER
       ZapolnitComponent,
       SpisokFazComponent,
       SpisokKazneyComponent,
       SpisokSobytiyComponent,
       SpisokTestovComponent,
       ZapisSobytiyComponent,
+      // QUERY AREA
       StolComponent,
       ZaprosComponent,
+      // ADMINA AREA
       SmenaComponent,
       AdminEtapComponent,
       AdminSkhemaComponent,
       AdminBumagaComponent,
       AdminTovarComponent,
       AdminUstroystvoComponent,
-      AdminOtkrytkaComponent
+      AdminOtkrytkaComponent,
+      // PIPES
+      ResultEnumPipe,
+      ExpectedEnumPipe,
+      TapActionEnumPipe,
+      TapActionToArrayPipe,
+      TesterNamePipe
    ],
    imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
@@ -134,40 +126,17 @@ import { SobytiyEffects } from './zapolnit/spisok-sobytiy/spisok-sobytiy.effects
     ApiAuthorizationModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
-      { path: 'todo', component: TodoComponent, canActivate: [AuthorizeGuard] }, // Todo list
+      { path: 'todo', component: TodoComponent, canActivate: [AuthorizeGuard] },
       { path: 'versionOne', component: TapLoggerComponent, canActivate: [AuthorizeGuard] },
-      { path: 'logger', component: TapperComponent, pathMatch: 'full', canActivate: [AuthorizeGuard] }, // Taplogger
-      { path: 'zapolnit', component: ZapolnitComponent, canActivate: [AuthorizeGuard] },
-      { path: 'stol', component: StolComponent, canActivate: [AuthorizeGuard] }, // Exporter/Query
-      { path: 'smena', component: SmenaComponent, canActivate: [AuthorizeGuard] }, // Admin
+      { path: 'versionTwo', component: TapperComponent, pathMatch: 'full', canActivate: [AuthorizeGuard] },
+      { path: 'versionThree', component: ZapolnitComponent, canActivate: [AuthorizeGuard] },
+      { path: 'stol', component: StolComponent, canActivate: [AuthorizeGuard] },
+      { path: 'smena', component: SmenaComponent, canActivate: [AuthorizeGuard] },
     ]),
     CommonModule,
     ReactiveFormsModule,
     BrowserAnimationsModule,
-    MatButtonModule,
-    MatIconModule,
-    MatInputModule,
-    MatSelectModule,
-    MatFormFieldModule,
-    MatListModule,
-    MatRadioModule,
-    MatButtonToggleModule,
-    MatExpansionModule,
-    MatSlideToggleModule,
-    MatAutocompleteModule,
-    MatTableModule,
-    MatPaginatorModule,
-    MatSortModule,
-    MatProgressSpinnerModule,
-    MatDatepickerModule,
-    MatNativeDateModule,
-    MatCheckboxModule,
-    MatTableExporterModule,
-    MatBadgeModule,
-    MatMenuModule,
-    MatSidenavModule,
-    MatDialogModule,
-    MatSnackBarModule,
+    MaterialModule,
     ModalModule.forRoot(),
     ButtonsModule.forRoot(),
     TimepickerModule.forRoot(),
